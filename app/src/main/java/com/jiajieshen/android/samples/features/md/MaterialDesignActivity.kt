7c1b6ec14eba608481301b3c11e3d115e0a9d6bb
xin <cong@xin.pm>
2017-06-02 16:03:59 +0800

    BottomNavActivity

--- app/src/main/java/com/jiajieshen/android/samples/features/md/MaterialDesignActivity.kt
+++ app/src/main/java/com/jiajieshen/android/samples/features/md/MaterialDesignActivity.kt
@@ -2,7 +2,7 @@ package com.jiajieshen.android.samples.features.md
 
 import com.jiajieshen.android.samples.R
 import com.jiajieshen.android.samples.features.main.IntentActivity
-import com.jiajieshen.android.samples.features.tool.ToolsActivity
+import com.jiajieshen.android.samples.features.md.bottomnav.BottomNavActivity
 import com.jiajieshen.android.samples.model.IntentItem
 import kotlinx.android.synthetic.main.activity_intent.*
 import java.util.*
@@ -16,7 +16,7 @@ class MaterialDesignActivity : IntentActivity() {
 
     override fun getIntentItems(): List<IntentItem> {
         val intentItemList = ArrayList<IntentItem>()
-        intentItemList.add(IntentItem(getString(R.string.tools), ToolsActivity::class.java.name))
+        intentItemList.add(IntentItem(getString(R.string.bottom_navigation), BottomNavActivity::class.java.name))
         return intentItemList
     }
 
---
+++ app/src/main/java/com/jiajieshen/android/samples/features/md/bottomnav/BottomNavActivity.kt
@@ -0,0 +1,44 @@
+package com.jiajieshen.android.samples.features.md.bottomnav
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import com.jiajieshen.android.samples.R
+
+class BottomNavActivity : AppCompatActivity() {
+
+    private var mTextMessage: TextView? = null
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                mTextMessage!!.setText(R.string.title_home)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+                mTextMessage!!.setText(R.string.title_dashboard)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                mTextMessage!!.setText(R.string.title_notifications)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications_2 -> {
+                mTextMessage!!.setText(R.string.title_notifications)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bottom_nav)
+
+        mTextMessage = findViewById(R.id.message) as TextView
+        val navigation = findViewById(R.id.navigation) as BottomNavigationView
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+}
