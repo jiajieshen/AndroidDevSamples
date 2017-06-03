21850cd8827c30ef284f00e6f3264b3c075b35b5
xin <cong@xin.pm>
2017-06-03 23:08:15 +0800

    feature: Show snackbar over BottomNavigationView in BottomNavActivity

--- app/src/main/java/com/jiajieshen/android/samples/features/md/bottomnav/BottomNavActivity.kt
+++ app/src/main/java/com/jiajieshen/android/samples/features/md/bottomnav/BottomNavActivity.kt
@@ -2,9 +2,15 @@ package com.jiajieshen.android.samples.features.md.bottomnav
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.TextView
 import com.jiajieshen.android.samples.R
+import kotlinx.android.synthetic.main.activity_bottom_nav.*
+
 
 class BottomNavActivity : AppCompatActivity() {
 
@@ -14,6 +20,13 @@ class BottomNavActivity : AppCompatActivity() {
         when (item.itemId) {
             R.id.navigation_home -> {
                 mTextMessage!!.setText(R.string.title_home)
+                val snackbar = Snackbar.make(vgContentContainer, R.string.title_home, Snackbar.LENGTH_SHORT)
+                val vlp: ViewGroup.LayoutParams = snackbar.view.layoutParams
+                val flp = FrameLayout.LayoutParams(vlp.width, vlp.height)
+                flp.gravity = Gravity.BOTTOM;
+                flp.bottomMargin = navigation.height
+                snackbar.view.layoutParams = flp
+                snackbar.show()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
