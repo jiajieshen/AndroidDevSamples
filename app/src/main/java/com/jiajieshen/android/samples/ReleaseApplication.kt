99f0493e82a025fcf5c09d797056a7c83e5e06be
xin <cong@xin.pm>
2017-06-20 22:39:35 +0800

    features: add firebase analytics and bugly

---
+++ app/src/main/java/com/jiajieshen/android/samples/ReleaseApplication.kt
@@ -0,0 +1,30 @@
+package com.jiajieshen.android.samples
+
+import android.app.Application
+import android.text.TextUtils
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.jiajieshen.android.library.util.ProcessUtil
+import com.tencent.bugly.crashreport.CrashReport
+
+
+/**
+ * Created by xin on 6/20/17.
+ */
+class ReleaseApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // get current process name
+        val processName: String? = ProcessUtil.getProcessName(android.os.Process.myPid())
+        if (TextUtils.equals(processName, packageName)) {
+
+            // init Bugly
+            CrashReport.initCrashReport(this, BuildConfig.BUGLY_APP_ID, BuildConfig.DEBUG)
+
+            // init firebase
+            FirebaseAnalytics.getInstance(this)
+        }
+
+    }
+}
